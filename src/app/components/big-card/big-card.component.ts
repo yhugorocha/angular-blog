3b3823aa2bbef74db810ebea352b2006badaaae1
Hugo Silva <yhugorocha@gmail.com>
2024-08-16 13:27:11 -0300

    feat: add finalização do componente de big card.

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent{
+
+  @Input() photoCover:String="";
+  @Input() datePublication:String="";
+  @Input() cardTitle:String="";
+  @Input() cardDescription:String="";
+  @Input() id:String= "0";
+
 
 }
