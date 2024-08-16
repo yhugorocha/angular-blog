cf69476a26fd842b15fde74dff3d53a8aa47910f
Hugo Silva <yhugorocha@gmail.com>
2024-08-16 13:26:16 -0300

    feat: add arquivo com dados fakes localmente.

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photoCover": "https://www.totvs.com/wp-content/uploads/2023/04/tecnologias-digitais.jpg",
+    "datePublication": "Janeiro 12, 2024",
+    "title": "Qui voluptate magni non delectus placeat sit unde corporis qui soluta nesciun",
+    "description": "Lorem ipsum dolor sit amet. Qui reiciendis reiciendis 33 laborum vero et voluptate quia ut magnam voluptate nam assumenda odio id eaque ducimus. Quo voluptatem dolor qui commodi omnis qui error officiis. At autem architecto et possimus optio eum ratione eaque. Ea quis saepe ut harum quia et consectetur quod et galisum consequatur sit deserunt sunt est odit quia."
+  },
+  {
+    "id": "2",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSedm-aYMSVW5BaJfcx3b0H5mRgR4BQ-SXdXw&s",
+    "datePublication": "Janeiro 11, 2024",
+    "title": "Qui perspiciatis assumenda aut consequuntur",
+    "description": "Lorem ipsum dolor sit amet. Qui reiciendis reiciendis 33 laborum vero et voluptate quia ut magnam voluptate nam assumenda odio id eaque ducimus. Quo voluptatem dolor qui commodi omnis qui error officiis. At autem architecto et possimus optio eum ratione eaque. Ea quis saepe ut harum quia et consectetur quod et galisum consequatur sit deserunt sunt est odit quia."
+  },
+  {
+    "id": "3",
+    "photoCover": "https://img.freepik.com/vetores-gratis/fundo-futurista-de-tecnologia-gradiente_23-2149122416.jpg",
+    "datePublication": "Janeiro 10, 2024",
+    "title": "Qui commodi nostrum qui eligendi porro ab similique",
+    "description": "Lorem ipsum dolor sit amet. Qui reiciendis reiciendis 33 laborum vero et voluptate quia ut magnam voluptate nam assumenda odio id eaque ducimus. Quo voluptatem dolor qui commodi omnis qui error officiis. At autem architecto et possimus optio eum ratione eaque. Ea quis saepe ut harum quia et consectetur quod et galisum consequatur sit deserunt sunt est odit quia."
+  },
+  {
+    "id": "4",
+    "photoCover": "https://futurodasaude.com.br/wp-content/uploads/2021/07/AdobeStock_174917858.jpeg",
+    "datePublication": "Janeiro 09, 2024",
+    "title": "Voluptatibus ea deserunt consequuntur?",
+    "description": "Lorem ipsum dolor sit amet. Qui reiciendis reiciendis 33 laborum vero et voluptate quia ut magnam voluptate nam assumenda odio id eaque ducimus. Quo voluptatem dolor qui commodi omnis qui error officiis. At autem architecto et possimus optio eum ratione eaque. Ea quis saepe ut harum quia et consectetur quod et galisum consequatur sit deserunt sunt est odit quia."
+  }
+]
